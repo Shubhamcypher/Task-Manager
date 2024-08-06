816c17ef8400222c32ac39bbcad2aa84122ec59e
shubhamcypher <shubhamdevo23@gmail.com>
2024-08-07 04:55:27 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors")
 app.use(cors(
     {
-        origin:['https://task-manager-psi-sage.vercel.app','https://task-manager-frontend-azure.vercel.app','http://localhost:3000'],
+        origin:['https://task-manager-psi-sage.vercel.app','https://task-manager-frontend-azure.vercel.app','http://localhost:3000','http://localhost:8000'],
         methods:['GET','PUT','PATCH','DELETE','POST'],
         credentials:true
     }
